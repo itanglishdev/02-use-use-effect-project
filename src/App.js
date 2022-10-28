1f8556242c658587f25cdf1d48b738f67d7c5d69
Chris <chrishethertonweb@gmail.com>
2022-10-28 19:46:56 +0200

    user contaxt stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import AuthContext from './store/auth-context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -29,11 +29,17 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider 
+          value={{
+            isLoggedIn : isLoggedIn,
+            onLogout: logoutHandler
+        }} >
+        <MainHeader  onLogout={logoutHandler} />
+          <main>
+            {!isLoggedIn && <Login onLogin={loginHandler} />}
+            {isLoggedIn && <Home onLogout={logoutHandler} />}
+          </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn : false
+    isLoggedIn : false,
+    onLogout: () => {}
 })
+const AuthContextProvider = (props) => {
+    return <AuthContext.Provider>{props.children}</AuthContext.Provider>
 
-export default AuthContext
+}
+
+export default AuthContext 
